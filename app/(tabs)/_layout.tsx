f33926cacb32bf022a782733d41f3ba39c25bd7a
Theo Schnegg <s2410456029@fhooe.at>
2026-06-28 16:56:30 +0200

    Automaten hinzufügen, Bestand, Startseite

--- app/(tabs)/_layout.tsx
+++
@@ -1,6 +0,0 @@
-// Wurzel-Layout. Versteckt den Stack-Header, da die Tabs eigene Header haben.
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
-}
